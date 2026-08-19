c5477b74a3942045f2ef7fab1714831738ff2fe4
Derek Scott <598517+johnny13@users.noreply.github.com>
2026-08-19 12:01:07 -0500

    feat: Testing Framework and Initial ViTest added to validate cookie fetching.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
@@ -9,5 +10,9 @@ export default defineConfig({
   build: {
     minify: 'terser',
     cssMinify: true,
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
   },
 });
