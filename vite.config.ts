ca0e7d7d4782700cbe8191b0b2b7c17b60fd7ffb
Derek Scott <598517+johnny13@users.noreply.github.com>
2026-08-19 12:03:36 -0500

    fix: vite config updated to add testing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
